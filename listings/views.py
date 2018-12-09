bbbf4383cd32f4978a831dece87e753446255905
Fernando Cabello <amigo2@hotmail.com>
2018-12-09 18:05:54 +0100

    Dyanmic content, still in about

--- listings/views.py
+++ listings/views.py
@@ -1,19 +1,32 @@
 from django.shortcuts import render
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+ 
 from .models import Listing
 
-def index(request):
-    listings = Listing.objects.all()
 
+# indek view
+def index(request):
+    # gather all ojects in listings
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    # pagination
+    # max objevt per page
+    paginator = Paginator(listings,3)
+    page = request.GET.get('page')
+    paged_listing = paginator.get_page(page)
+
+    # define a key pair var
     context={
-        'listings' : listings
+        'listings' : paged_listing
 
     }
-
+    # return and render listing.html
     return render(request, 'listings/listings.html', context)
 
+# linsting view
 def listing(request, listing_id):
     return render(request, 'listings/listing.html')
 
+# search view
 def search(request):
     return render(request, 'listings/search.html')
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
+from listings.models import Listing
+from brokers.models import Broker
 
 
 def index(request):
+    # gather all ojects in listings
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+
+    # pagination
+    # max objevt per page
+    paginator = Paginator(listings,3)
+    page = request.GET.get('page')
+    paged_listing = paginator.get_page(page)
+
+    # define a key pair var
+    context={
+        'listings' : paged_listing
+
+    }
+
     #connected to the template index.html
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
     #connected to the template about.html
-    return render(request, 'pages/about.html')
+    brokers = Broker.objects.order_by('hire_date')
+
+    #get MVP
+    mvp_brokers = Broker.objects.all().filter(is_mvp=True)
+
+    contex = {
+        'brokers':brokers,
+        'mvp_brokers':brokers,
+    }
+
+    #return
+    return render(request, 'pages/about.html', contex)
